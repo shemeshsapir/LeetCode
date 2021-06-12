3d672380c142d4cef11541936fb81a558c367aad
Sapir Turgeman <39224974+shemeshsapir@users.noreply.github.com>
2021-06-12 21:22:55 +0300

    Create Product of Array Except Self.js

---
+++ Medium/Product of Array Except Self.js
@@ -0,0 +1,51 @@
+/*Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+    var left = [];
+    var right = [];
+    var answer = [];
+    var len = nums.length;
+    
+    left[0]=1;
+    for(let i=1; i<len; i++)
+        {
+            left[i]=nums[i - 1] * left[i - 1];
+        }
+    right[len-1]=1;
+    for(let i=len-2 ;i>=0;i--)
+        {
+            right[i]=nums[i + 1] * right[i + 1];
+        }
+    for(let i=0; i<len; i++)
+        {
+            answer[i] = left[i]*right[i];
+        }
+    return answer;
+};
